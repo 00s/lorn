129504bdde8aa1776c212d66a11253bca48103a4
Lucas Farias <adf.melo@gmail.com>
2014-11-09 13:36:09 -0500

    adds blur filter for the Tree images based on parallax logical depth and caches filter

--- Scripts/Tree.js
+++ Scripts/Tree.js
@@ -1,12 +1,18 @@
 var Tree = (function () {
 	function Tree (x, y, parallax) {
 
-		this.PARALLAX_FACTOR = 3;
+		this.PARALLAX_FACTOR = 4.5;
 		this.parallax = parallax;
 
         this.image = new createjs.Bitmap(queue.getResult("tree"));
         this.width = this.image.getBounds().width;
         this.height = this.image.getBounds().height;
+
+        var blur = new createjs.BlurFilter((1.2 - parallax) *15,(1.2 - parallax) *15,0.5);
+        this.image.filters = [blur];
+
+        this.image.cache(0,0,this.width, this.height);
+
         this.image.regX = this.width * 0.5;
         this.image.regY = this.height;
 
